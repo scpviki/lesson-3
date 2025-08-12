4c248cb00bc418ee35e10da477da5d2e706c0cae
scpviki <arun.letmein@gmail.com>
2025-08-12 11:50:39 +0400

    NEWCODE

---
+++ activity2.py
@@ -0,0 +1,14 @@
+#typecasting
+car_weight=890.90
+age=10
+name="VIKRAM"
+a=True
+print("type of car weight is ",type(car_weight))
+print("type of age is ",type(age))
+print("type of name is ",type(name))
+print("type of a is ",type(a))
+
+car_weight=int(car_weight)
+age=str(age)
+print("type of car weight is ",type(car_weight))
+print("type of age is ",type(age))
